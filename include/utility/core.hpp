70f6babbae1f092e5a1fca39a55cbb70f3cc9d25
岡野智之 <84751283+okano-tomoyuki@users.noreply.github.com>
2023-10-02 05:54:29 +0900

    Update core.hpp
    1.Add copy function
    2.Add show function,
    3.Add DEBUG_PRINT MACRO

--- include/utility/core.hpp
+++ include/utility/core.hpp
@@ -3,8 +3,11 @@
  * @author Okano Tomoyuki (tomoyuki.okano@tsuneishi.com)
  * @brief 
  * @version 0.1
- * @date 2023-09-29
- * 
+ * @date 2023-09-29 Created by Okano Tomoyuki.
+ * @date 2023-10-02 Updated by Okano Tomoyuki.
+ * @n 1.Add copy function
+ * @n 2.Add show function,
+ * @n 3.Add DEBUG_PRINT MACRO
  * @copyright Copyright (c) 2023
  * 
  */
@@ -17,6 +20,12 @@
 #include <cmath>
 #include <string>
 #include <sstream>
+#include <typeinfo>
+
+#ifndef _DEBUG_MODE_
+    #define _DEBUG_MODE_ 1 
+    #define DEBUG_PRINT(var) if(_DEBUG_MODE_){do{std::cout << #var << "[" << typeid(var).name() << "]:\n";Utility::show(var);}while(0);}
+#endif
 
 namespace Utility
 {
@@ -53,6 +62,16 @@ T limit(const T& value, const T& lower_limit, const T& upper_limit) noexcept
     return std::max(std::min(value, upper_limit), lower_limit);
 }
 
+double adjust_pi(const double& value) noexcept
+{
+    double result = value;
+    while(result>M_PI)
+        result -= 2*M_PI;
+    while(result<-M_PI)
+        result += 2*M_PI;
+    return result;
+}
+
 double adjust_180(const double& value) noexcept
 {
     double result = value;
@@ -63,13 +82,12 @@ double adjust_180(const double& value) noexcept
     return result;
 }
 
-std::string concat(const std::vector<std::string>& origin, const char& separator=',') noexcept
+template<typename T>
+std::string concat(const std::vector<T>& origin, const char& separator=',') noexcept
 {
     std::string result;
     for (const auto& str : origin)
-    {
-        result += str + separator;
-    }
+        result += std::to_string(str) + separator;
     result.pop_back(); // erase separator character placed in end.
     return result;
 }
@@ -83,6 +101,7 @@ std::vector<std::string> split(const std::string& origin, const char& separator=
     {
         result.push_back(element);
     }
+
     return result;
 }
 
@@ -102,6 +121,63 @@ std::vector<int> range(const int& start, const int& end, const int& interval=1) 
         i = start + interval * i;
     }
     return result;
+}
+
+template<size_t ROW, size_t COL, typename T>
+bool copy(T (&source)[ROW][COL], std::vector<std::vector<T>>& target, const size_t& row_size=ROW, const size_t& column_size=COL) noexcept
+{
+    if(!(row_size>0 && column_size>0 && row_size<=ROW && column_size<=COL))
+        return false;
+    
+    target.resize(row_size);
+    for (auto& row : target)
+        row.resize(column_size);
+
+    for (const auto& i : range(row_size))
+        for (const auto& j : range(column_size))
+            target[i][j] = source[i][j];
+
+    return true;
+}
+
+template<size_t ROW, size_t COL, typename T>
+bool copy(std::vector<std::vector<T>>& source, T (&target)[ROW][COL], const size_t& row_size=ROW, const size_t& column_size=COL) noexcept
+{
+    if(!(row_size>0 && column_size>0 && row_size<=ROW && column_size<=COL))
+        return false;
+    
+    target.resize(row_size);
+    for (auto& row : target)
+        row.resize(column_size);
+
+    for (const auto& i : range(row_size))
+        for (const auto& j : range(column_size))
+            target[i][j] = source[i][j];
+
+    return true;
+}
+
+template<typename T> 
+void show(T e) noexcept
+{
+    std::cout << e << std::endl;
+}
+
+template<typename T> 
+void show(const std::vector<T>& v) noexcept
+{
+    std::cout << "{" << concat(v) << "}" << std::endl;
+}
+
+template<typename T> 
+void show(const std::vector<std::vector<T>>& vv) noexcept
+{ 
+    std::string s; 
+    for(const auto& v : vv)
+        s += " {" + concat(v) + "},\n";
+    s.pop_back();
+    s.pop_back();
+    std::cout << "{\n" << s << "\n}" << std::endl;
 }
 
 } // Utility
